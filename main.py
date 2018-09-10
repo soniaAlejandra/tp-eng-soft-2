66b7f1eab8ed5ff91a0a78862f7209ec84593dfa
Vitor Paisante <vitor.paisante@sambatech.com.br>
2018-09-09 23:16:23 -0300

    Added database

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+import sqlite3
 from flask import Flask
 app = Flask(__name__)
+
+DATABASE = 'database.db'
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/estados/<nome>')
+def get_capital(nome):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT capital FROM estados WHERE nome = '{}';".format(nome))
+    linha = cursor.fetchone();
+    conn.close()
+    return 'Capital: {}'.format(linha[0])
 
 
 if __name__ == '__main__':
